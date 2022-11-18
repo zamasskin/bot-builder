5c7da88116faff5433fd6557a7d8eb9ba6d971e8
Alexey Zamasskin <zamasskin@yandex.ru>
2022-11-18 18:04:43 +0300

    Родключил чакру

---
+++ components/flow/Flow.tsx
@@ -0,0 +1,54 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "reactflow";
+import type { Connection } from "reactflow";
+// 👇 you need to import the reactflow styles
+import "reactflow/dist/style.css";
+import BotSender from "./nodes/BotSender/BotSender";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "2" },
+    type: "bot-sender",
+  },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+const nodeTypes = {
+  "bot-sender": BotSender,
+};
+
+export default function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      nodeTypes={nodeTypes}
+    >
+      <MiniMap />
+      <Controls />
+      <Background />
+    </ReactFlow>
+  );
+}
---
+++ components/flow/nodes/BotSender/BotSender.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+function BotSender() {
+  return (
+    <div>
+      <span>bot sender</span>
+    </div>
+  );
+}
+
+export default memo(BotSender);
