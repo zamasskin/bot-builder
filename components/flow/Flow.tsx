782fc8e2cb62433e8e62256db002d3f5ef555582
Alexey Zamasskin <zamasskin@yandex.ru>
2022-11-21 12:39:16 +0300

    Примерная верстка для блока сообщений

--- components/flow/Flow.tsx
+++ components/flow/Flow.tsx
@@ -13,16 +13,25 @@ import "reactflow/dist/style.css";
 import BotSender from "./nodes/BotSender/BotSender";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "1", position: { x: 100, y: 10 }, data: { label: "1" } },
   {
     id: "2",
-    position: { x: 0, y: 100 },
+    position: { x: 200, y: 100 },
     data: { label: "2" },
     type: "bot-sender",
+    // dragHandle: ".custom-drag-handle",
   },
+
+  // { id: "3", position: { x: 100, y: 300 }, data: { label: "2" } },
 ];
 
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+  },
+];
 
 const nodeTypes = {
   "bot-sender": BotSender,
--- components/flow/nodes/BotSender/BotSender.tsx
+++ components/flow/nodes/BotSender/BotSender.tsx
@@ -1,10 +1,138 @@
-import { memo } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Button,
+  Text,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Box,
+  Divider,
+  VStack,
+} from "@chakra-ui/react";
+import { Handle, Position } from "reactflow";
+import { DndContext } from "@dnd-kit/core";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
+import { memo, useState } from "react";
+import { CSS } from "@dnd-kit/utilities";
+
+// Icons
+import { BsChat } from "react-icons/bs";
+
+function SortableItem(props: any) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div>hello</div>
+    </div>
+  );
+}
 
 function BotSender() {
+  const [items, setItems] = useState([1, 2, 3]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div>
-      <span>bot sender</span>
-    </div>
+    <Box>
+      <Handle type="target" position={Position.Top} />
+      <Card bg="white" borderTopRadius="xl" onClick={onOpen}>
+        <CardHeader
+          bg="gray.100"
+          borderTopRadius="xl"
+          className="custom-drag-handle"
+        >
+          <Heading size="sm">Цепочка сообщений</Heading>
+        </CardHeader>
+        <VStack spacing="2" my="2">
+          <Box w="full" position="relative">
+            <Handle
+              type="source"
+              position={Position.Right}
+              id="menu-source-0"
+            />
+            <Box
+              mx="2"
+              padding="2"
+              boxShadow="xs"
+              borderRadius="md"
+              bg="teal.400"
+            >
+              <Text color="white">Меню 1</Text>
+            </Box>
+          </Box>
+
+          <Box w="full" position="relative">
+            <Handle
+              type="source"
+              position={Position.Right}
+              id="menu-source-2"
+            />
+            <Box
+              mx="2"
+              padding="2"
+              boxShadow="xs"
+              borderRadius="md"
+              bg="teal.400"
+            >
+              <Text color="white">Меню 2</Text>
+            </Box>
+          </Box>
+        </VStack>
+        <Divider />
+        <VStack spacing="2" my="2">
+          <Box w="full" pos="relative">
+            <Handle
+              type="source"
+              position={Position.Right}
+              id="source-handle-0"
+            />
+            <Box
+              mx="2"
+              padding="2"
+              boxShadow="xs"
+              borderRadius="md"
+              bg="gray.500"
+            >
+              <Text color="white">Любая фраза</Text>
+            </Box>
+          </Box>
+          <Box w="full" pos="relative">
+            <Handle
+              type="source"
+              position={Position.Right}
+              id="source-handle-1"
+            />
+            <Box mx="2" padding="2" boxShadow="xs" borderRadius="md">
+              <Text>Продолжить</Text>
+            </Box>
+          </Box>
+        </VStack>
+      </Card>
+      <Drawer isOpen={isOpen} onClose={onClose} placement="right">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <Text>Some contents...</Text>
+            <Text>Some contents...</Text>
+            <Text>Some contents...</Text>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
   );
 }
 
