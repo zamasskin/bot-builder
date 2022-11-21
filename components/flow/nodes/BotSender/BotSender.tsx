b4d72d746b15f6f289d411122667097ddf286247
Alexey Zamasskin <zamasskin@yandex.ru>
2022-11-21 18:43:30 +0300

    Верстка

--- components/flow/nodes/BotSender/BotSender.tsx
+++ components/flow/nodes/BotSender/BotSender.tsx
@@ -21,6 +21,15 @@ import {
   HStack,
   Wrap,
   WrapItem,
+  DrawerCloseButton,
+  Select,
+  Textarea,
+  FormLabel,
+  FormControl,
+  InputGroup,
+  InputLeftAddon,
+  Input,
+  Center,
 } from "@chakra-ui/react";
 import { Handle, Position } from "reactflow";
 import { DndContext } from "@dnd-kit/core";
@@ -122,10 +131,13 @@ function BotSender() {
       <Drawer isOpen={isOpen} onClose={onClose} placement="right" size="md">
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">
+            Настойка цепочки сообщений
+          </DrawerHeader>
           <DrawerBody>
-            <VStack spacing="2">
-              <Box w="full">
+            <VStack spacing="5">
+              <VStack spacing="2" w="full" align="left">
                 <Heading as="div" size="sm">
                   Цепочка сообщений
                 </Heading>
@@ -147,15 +159,52 @@ function BotSender() {
                     Документ из CRM
                   </AddItemBtn>
                 </AddItemBtnWrap>
-              </Box>
+              </VStack>
               <Divider />
-              <Box w="full">
-                <Text>Some contents...</Text>
-                <Text>Some contents...</Text>
-              </Box>
+              <VStack spacing="4" w="full" align="left">
+                <Heading as="div" size="sm">
+                  Валидация
+                </Heading>
+                <Select>
+                  <option value="">Нет</option>
+                  <option value="text">Текст</option>
+                  <option value="number">Цифры</option>
+                  <option value="symbol">Буквы</option>
+                  <option value="symbolAndNumber">Буквы и цифры</option>
+                  <option value="range">Диапазон чисел</option>
+                  <option value="date">Дата и время</option>
+                  <option value="phone">Номер телефона</option>
+                  <option value="email">Email</option>
+                  <option value="url">URL</option>
+                  <option value="image">Картинка</option>
+                  <option value="audio">Аудио</option>
+                  <option value="video">Видео</option>
+                  <option value="file">Файл</option>
+                  <option value="regex">Регулярное выражение</option>
+                </Select>
+                <FormControl>
+                  <FormLabel>Сообщение об ошибке валидации</FormLabel>
+                  <Textarea placeholder="Оставьте пустым для отправки системного сообщения об ошибке"></Textarea>
+                </FormControl>
+              </VStack>
+              <Divider />
+              <VStack spacing="4" w="full" align="left">
+                <Heading as="div" size="sm">
+                  Меню
+                </Heading>
+                <Box>
+                  <InputGroup>
+                    <InputLeftAddon>...</InputLeftAddon>
+                    <Input type="tel" isDisabled value="Любая фраза" />
+                  </InputGroup>
+                </Box>
+                <Center>
+                  <Button size="sm" colorScheme="blue" leftIcon={<FaPlus />}>
+                    Добавить еще
+                  </Button>
+                </Center>
+              </VStack>
             </VStack>
-
-            <Text>Some contents...</Text>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
