16ef5481bd3b95ef1c14da5b9732da451318c070
Alexey Zamasskin <zamasskin@yandex.ru>
2022-11-21 14:48:53 +0300

    Верстка элемента

--- components/flow/nodes/BotSender/BotSender.tsx
+++ components/flow/nodes/BotSender/BotSender.tsx
@@ -39,22 +39,6 @@ import {
   FaRegFileWord,
 } from "react-icons/fa";
 
-function SortableItem(props: any) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: props.id });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <div>hello</div>
-    </div>
-  );
-}
-
 function AddItemBtn({
   icon,
   children,
@@ -83,8 +67,19 @@ function AddItemBtnWrap({
   );
 }
 
+function HandleItem(props: HandleItemProps) {
+  const { id, caption, color = "black", bg } = props;
+  return (
+    <Box w="full" position="relative">
+      <Handle type="source" position={Position.Right} id={id} />
+      <Box mx="2" padding="2" boxShadow="xs" borderRadius="md" bg={bg}>
+        <Text color={color}>{caption}</Text>
+      </Box>
+    </Box>
+  );
+}
+
 function BotSender() {
-  const [items, setItems] = useState([1, 2, 3]);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
@@ -99,68 +94,29 @@ function BotSender() {
           <Heading size="sm">Цепочка сообщений</Heading>
         </CardHeader>
         <VStack spacing="2" my="2">
-          <Box w="full" position="relative">
-            <Handle
-              type="source"
-              position={Position.Right}
-              id="menu-source-0"
-            />
-            <Box
-              mx="2"
-              padding="2"
-              boxShadow="xs"
-              borderRadius="md"
-              bg="teal.400"
-            >
-              <Text color="white">Меню 1</Text>
-            </Box>
-          </Box>
-
-          <Box w="full" position="relative">
-            <Handle
-              type="source"
-              position={Position.Right}
-              id="menu-source-2"
-            />
-            <Box
-              mx="2"
-              padding="2"
-              boxShadow="xs"
-              borderRadius="md"
-              bg="teal.400"
-            >
-              <Text color="white">Меню 2</Text>
-            </Box>
-          </Box>
+          <HandleItem
+            caption="Меню 1"
+            color="white"
+            bg="teal.400"
+            id="menu-source-0"
+          />
+          <HandleItem
+            caption="Меню 2"
+            color="white"
+            bg="teal.400"
+            id="menu-source-1"
+          />
         </VStack>
         <Divider />
         <VStack spacing="2" my="2">
-          <Box w="full" pos="relative">
-            <Handle
-              type="source"
-              position={Position.Right}
-              id="source-handle-0"
-            />
-            <Box
-              mx="2"
-              padding="2"
-              boxShadow="xs"
-              borderRadius="md"
-              bg="gray.500"
-            >
-              <Text color="white">Любая фраза</Text>
-            </Box>
-          </Box>
-          <Box w="full" pos="relative">
-            <Handle
-              type="source"
-              position={Position.Right}
-              id="source-handle-1"
-            />
-            <Box mx="2" padding="2" boxShadow="xs" borderRadius="md">
-              <Text>Продолжить</Text>
-            </Box>
-          </Box>
+          <HandleItem
+            caption="Любая фраза"
+            color="white"
+            bg="gray.500"
+            id="source-handle-0"
+          />
+
+          <HandleItem caption="Продолжить" bg="white" id="source-handle-1" />
         </VStack>
       </Card>
       <Drawer isOpen={isOpen} onClose={onClose} placement="right" size="md">
@@ -205,6 +161,13 @@ function BotSender() {
       </Drawer>
     </Box>
   );
+}
+
+interface HandleItemProps {
+  caption: string;
+  color?: string;
+  bg: string;
+  id: string;
 }
 
 export default memo(BotSender);
